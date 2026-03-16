37df581a60241151931d2fd2f726eff9a0cf0c43
anandhuuu03 <anandhukrishnatb02@gmail.com>
2026-03-16 10:15:28 +0530

    dd

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// This checks if the site is running on Vercel. 
+// If it is, use "/" (root). If not, use "/germanvocabs" (GitHub).
+const baseName = window.location.hostname.includes('vercel.app') 
+  ? '/' 
+  : '/germanvocabs';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/germanvocabs">
+    <BrowserRouter basename={baseName}>
       <App />
     </BrowserRouter>
   </StrictMode>
